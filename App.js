a08276fbcfed5916678644871ebb458ecc9622b5
Quyen Ha <quyenha2511@gmail.com>
2017-12-28 09:22:32 -0800

    add redux thunk to handle async log in

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './src/reducers';
 import firebase from 'firebase';
 import LoginForm from './src/components/LoginForm';
+import ReduxThunk from 'redux-thunk';
 
 export default class App extends React.Component {
   componentWillMount() {
@@ -22,7 +23,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         <LoginForm />
       </Provider>
     );
